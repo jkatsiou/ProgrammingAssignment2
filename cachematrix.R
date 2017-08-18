2a7d3729203afec7341757e7735c21bce3d1210e
jkatsiou <30498444+jkatsiou@users.noreply.github.com>
2017-08-18 14:48:30 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
 ## The two functions work together in order to create, store, search a cache of matrices and return their inverses. 
-## First function creates the cache (a list containing the matrix,its inverse and a set of functions), while second retrieves 
-## the inverese (if it exists) or If the inverse has not been previous calculated (m = null) it calculates, stores and displays it. 
+## The makeCacheMatrix function creates the cache (a list containing the matrix,its inverse and a set of functions), while cacheSolve retrieves 
+## the inverese (if it has been previously calculated) or If the inverse has not been previous calculated, it calculates, stores and displays it. 
 
 
 
+## The makeCacheMatrix function: 
 
-## The makeCacheMatrix function, accepts as input a matrix and returns an object which is a list containing the input matrix 
-## (in variable x), the inverse of input matrix (in variable m), as well as a set of functions which are used by function cacheSolve.
-## The functions included are: 
+## The makeCacheMatrix function, accepts as input a matrix and returns an object which is a list containing the input matrix  
+## (in variable x), the inverse of input matrix (in variable m), as well as a set of functions which are used by functions.
+## The functions of the list are used from cacheSolve, for the computation or storage of matrix inverse
 
-## The elements of the object (list) created are used from solveCache, either for the computation or storage of matrix inverse
+## The functions included in the list are: 
 
 
 ## set:it initializes values of x and m of its enclosing environment (list created by makeCacheMatrix) 
@@ -17,7 +18,7 @@
 ## get:Returns to cacheSolve the matrix x 
 
 ## setinverse:Gets the inverse which was caclulated by cacheSolve and stores it in variable m of its enclosing 
-##environment (list created by makeCacheMatrix) enviroment 
+##environment (list created by makeCacheMatrix)
  
 ## getinverse:returns the inverse of matrix x, that has been previously stored by the setinverse function. If m has not been previoulsy
 ## stored it returns the initial value of m which has been set to null  
@@ -46,18 +47,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-
-
+## The cacheSolve Function :
 
 ## In order cacheSolve to work, it has to receive as input a list that was previously created by the makeCacheMatrix function.
 
-## The cacheSolve function attempts to find a if an inverse has been previously created for the matrix included 
-## in the list passed to x by invoking the  $getinverse() element, which returns the stored inverse of the matrix. 
+## The cacheSolve function attempts to find if an inverse has been previously created for the matrix included 
+## in its argument (list x), by invoking the  $getinverse() element of the list. 
 
-## If this  returns a  value different than null (which means that an inverse has been previously calculated for the matrix)
+## If $getinverse returns a  value different than null (which means that an inverse has been previously calculated for the matrix)
 ## then it returns the inverse, prints the message "getting cached data" and stops the execution. 
 
-## Otherwise it calculates the inverse, displays and passes it to variable m of the (list) by invoking the $setinverse() element. 
+## Otherwise it calculates the inverse, displays and passes it to variable m of the list by invoking the $setinverse() element. 
 
 
 cacheSolve <- function(x, ...) {
